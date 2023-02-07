d76c095cb480b48e0ccd1deb42eb450d130c8866
Marvin Drees <marvin.drees@9elements.com>
2023-02-07 12:52:54 +0100

    style: replace rome with megalinter/prettier
    Signed-off-by: Marvin Drees <marvin.drees@9elements.com>

--- dist/invoke-action.js
+++ dist/invoke-action.js
@@ -1,44 +1,42 @@
-import * as childProcess from "child_process";
-import * as process from "process";
+import * as childProcess from 'child_process';
+import * as process from 'process';
 
-const VERSION = "0.0.1";
+const VERSION = '0.0.1';
 
 function chooseBinary() {
-    const platform = process.platform;
-    const arch = process.arch;
+  const platform = process.platform;
+  const arch = process.arch;
 
-    // Currently only supports Linux on X86_64
-    if (platform === "linux" && arch === "x64") {
-        return `action-linux-amd64-${VERSION}`;
-    }
+  // Currently only supports Linux on X86_64
+  if (platform === 'linux' && arch === 'x64') {
+    return `action-linux-amd64-${VERSION}`;
+  }
 
-    // If we're here the environment is incorrect
-    console.error(
-        `Unsupported platform (${platform}) and architecture (${arch})`,
-    );
-    return "";
+  // If we're here the environment is incorrect
+  console.error(`Unsupported platform (${platform}) and architecture (${arch})`);
+  return '';
 }
 
 function main() {
-    // Choose correct Go binary
-    const binary = chooseBinary();
-    if (binary === "") {
-        process.exit(1);
-    }
-    const actionBin = `${__dirname}/bin/${binary}`;
-    // Execute main Go programm
-    const spawnSyncReturns = childProcess.spawnSync(actionBin, {
-        stdio: "inherit",
-    });
-    // Check exit code
-    const status = spawnSyncReturns.status;
-    if (typeof status === "number") {
-        process.exit(status);
-    }
-    // If we're here, something went wrong
+  // Choose correct Go binary
+  const binary = chooseBinary();
+  if (binary === '') {
     process.exit(1);
+  }
+  const actionBin = `${__dirname}/bin/${binary}`;
+  // Execute main Go programm
+  const spawnSyncReturns = childProcess.spawnSync(actionBin, {
+    stdio: 'inherit'
+  });
+  // Check exit code
+  const status = spawnSyncReturns.status;
+  if (typeof status === 'number') {
+    process.exit(status);
+  }
+  // If we're here, something went wrong
+  process.exit(1);
 }
 
 if (require.main === module) {
-    main();
+  main();
 }
