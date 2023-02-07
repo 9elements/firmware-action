68cba982f2d67752d35f8545ac02e53641a1d64d
Marvin Drees <marvin.drees@9elements.com>
2023-02-07 15:05:51 +0100

    feat(coreboot): add initial dagger code
    Signed-off-by: Marvin Drees <marvin.drees@9elements.com>

--- action/coreboot.go
+++ action/coreboot.go
@@ -10,5 +10,28 @@ import (
 )
 
 func coreboot(ctx context.Context, action *githubactions.Action, client *dagger.Client) error {
-	return nil
+	// retrieve all action related context from environment
+	githubCtx, err := action.Context()
+	if err != nil {
+		return err
+	}
+	// set custom pipeline name
+	client = client.Pipeline("coreboot")
+	// get coreboot container
+	corebootContainer := client.Container().From("") //TODO: Get Dockerfile URL
+	// extract working directory from GitHub context
+	workspace := githubCtx.Workspace
+	// get reference to source code
+	src := client.Host().Directory(workspace)
+	// mount source directory into coreboot container
+	corebootContainer = corebootContainer.WithMountedDirectory("/src", src).WithWorkdir("/src")
+	// compile coreboot rom file
+	corebootContainer = corebootContainer.WithExec([]string{"make", "CPUS=$(nproc)"}) //TODO: configure target first
+	// get reference to generated coreboot rom file
+	romPath := "build/coreboot.rom"
+	rom := corebootContainer.File(romPath)
+	// export rom file from build container back to host
+	_, err = rom.Export(ctx, romPath)
+
+	return err
 }
