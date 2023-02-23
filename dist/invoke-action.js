7dfff9638fdaf586512e4474f593c27a971d7349
Marvin Drees <marvin.drees@9elements.com>
2023-02-23 08:50:00 +0100

    fix: satify js standard linter
    Signed-off-by: Marvin Drees <marvin.drees@9elements.com>

--- dist/invoke-action.js
+++ dist/invoke-action.js
@@ -1,42 +1,43 @@
-import * as childProcess from 'child_process';
-import * as process from 'process';
+import * as childProcess from 'child_process'
+import path from 'path'
+import * as process from 'process'
 
-const VERSION = '0.0.1';
+const VERSION = '0.0.1'
 
-function chooseBinary() {
-  const platform = process.platform;
-  const arch = process.arch;
+function chooseBinary () {
+  const platform = process.platform
+  const arch = process.arch
 
   // Currently only supports Linux on X86_64
   if (platform === 'linux' && arch === 'x64') {
-    return `action-linux-amd64-${VERSION}`;
+    return `action-linux-amd64-${VERSION}`
   }
 
   // If we're here the environment is incorrect
-  console.error(`Unsupported platform (${platform}) and architecture (${arch})`);
-  return '';
+  console.error(`Unsupported platform (${platform}) and architecture (${arch})`)
+  return ''
 }
 
-function main() {
+function main () {
   // Choose correct Go binary
-  const binary = chooseBinary();
+  const binary = chooseBinary()
   if (binary === '') {
-    process.exit(1);
+    process.exit(1)
   }
   const actionBin = path.join(`${__dirname}`, 'bin', `${binary}`)
   // Execute main Go program
   const spawnSyncReturns = childProcess.spawnSync(actionBin, {
     stdio: 'inherit'
-  });
+  })
   // Check exit code
-  const status = spawnSyncReturns.status;
+  const status = spawnSyncReturns.status
   if (typeof status === 'number') {
-    process.exit(status);
+    process.exit(status)
   }
   // If we're here, something went wrong
-  process.exit(1);
+  process.exit(1)
 }
 
 if (require.main === module) {
-  main();
+  main()
 }
