487374e2d41e8b661608682e8ac908631a3a1708
AtomicFS <vojtaveselych@seznam.cz>
2023-07-19 20:40:18 +0200

    refactor: move testing into script
    - this way, it can actually be parametric

---
+++ tests/coreboot/test_coreboot_4.19.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+# Environment variables
+export XGCCPATH='/repo/coreboot/util/crossgcc/xgcc/bin/'
+export BUILD_TIMELESS=1
+
+declare -a PAYLOADS=(
+	"seabios"
+	"seabios_coreinfo"
+	"seabios_nvramcui"
+)
+
+# Clone repo
+git clone --branch 4.19 --depth 1 https://review.coreboot.org/coreboot
+cd coreboot
+
+# Make
+for PAYLOAD in "${PAYLOADS[@]}"; do
+	make clean
+	make defconfig KBUILD_DEFCONFIG="../tests/coreboot/${PAYLOAD}.defconfig"
+	make -j "$(nproc)" || make
+done
