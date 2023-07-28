b4a070c223baf634f153edb126dd301653d772cc
AtomicFS <vojtaveselych@seznam.cz>
2023-07-28 20:21:53 +0200

    feat(dagger): add git commit sha and describe functions

---
+++ .dagger-ci/lib/git.py
@@ -0,0 +1,44 @@
+'''
+Functions to deal interface with local git repository
+'''
+
+import re
+import subprocess
+
+
+def git_get_latest_commit_sha_long() -> str:
+    '''
+    Assuming that current working directory is part of git repository,
+    get the sha of latest commit and return it.
+    '''
+    return subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip().decode()
+
+
+def git_get_latest_commit_sha_short() -> str:
+    '''
+    Assuming that current working directory is part of git repository,
+    get the sha of latest commit and return it.
+    '''
+    return git_get_latest_commit_sha_long()[:7]
+
+
+def git_describe() -> str:
+    '''
+    Run "git describe --tags"
+    '''
+    try:
+        return subprocess.check_output(['git', 'describe', '--tags']).strip().decode()
+    except subprocess.CalledProcessError:
+        return None
+
+
+def git_get_tag() -> str:
+    '''
+    Return a tag if currently tag is checked-out, else neturn None
+    '''
+    describe = git_describe()
+    if describe is None:
+        return None
+    if re.match(r'^v?\d+\.\d+(?:\.\d+)?$', describe):
+        return describe
+    return None
---
+++ .dagger-ci/tests/test_git.py
@@ -0,0 +1,36 @@
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=too-many-arguments
+
+import re
+import pytest
+from unittest.mock import patch
+
+from lib.git import *
+
+
+def test__git_get_latest_commit_sha_long():
+    assert re.match(r'^[a-z\d]{40}$', git_get_latest_commit_sha_long())
+
+
+def test__git_get_latest_commit_sha_short():
+    assert re.match(r'^[a-z\d]{7}$', git_get_latest_commit_sha_short())
+
+
+@pytest.mark.parametrize(
+    "describe, expected",
+    [
+        ('1.0', '1.0'),
+        ('v1.1', 'v1.1'),
+        ('v1.1.0', 'v1.1.0'),
+        ('1.1.0', '1.1.0'),
+        ('1.1.0-3-11dd3b9', None),
+        ('v1.1.0-3-11dd3b9', None),
+        ('lol', None),
+        (None, None),
+    ]
+)
+def test__git_get_tag(describe, expected):
+    with patch("lib.git.git_describe", wraps=git_describe) as mock_git_describe:
+        mock_git_describe.return_value = describe
+        assert git_get_tag() == expected
