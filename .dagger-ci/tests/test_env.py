753bc1c645f1f625f95a83a9ea483a6f7c7308f0
AtomicFS <vojtaveselych@seznam.cz>
2023-07-28 20:31:51 +0200

    feat(dagger): add function to read env var with fallback value

---
+++ .dagger-ci/tests/test_env.py
@@ -0,0 +1,28 @@
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=too-many-arguments
+
+import os
+import pytest
+
+from lib.env import *
+
+
+@pytest.mark.parametrize(
+    "list_of_maybe_existing_env_vars, fallback, presets, expected",
+    [
+        # No env vars set, should return fallfack
+        (['NONEXISTING_A', 'NONEXISTING_B'], '/tmp', [], '/tmp'),
+        # First env var set, should return that
+        (['NONEXISTING_A', 'NONEXISTING_B'], '/tmp', [['NONEXISTING_A', '/home']], '/home'),
+        # Two env vars set, should return first one
+        (['NONEXISTING_A', 'NONEXISTING_B'], '/tmp',
+         [['NONEXISTING_A', '/home'], ['NONEXISTING_B', '/usr']], '/home'),
+    ])
+def test__get_env_var_value(monkeypatch, list_of_maybe_existing_env_vars, fallback, presets, expected):
+    # Prepare environment
+    for pair in presets:
+        monkeypatch.setenv(pair[0], pair[1])
+    # Test
+    assert get_env_var_value(
+        list_of_maybe_existing_env_vars=list_of_maybe_existing_env_vars, fallback=fallback) == expected
