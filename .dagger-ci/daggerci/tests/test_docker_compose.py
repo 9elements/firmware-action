a38d35690c1f9ad28a270fe970f8c7217de75be3
AtomicFS <vojtaveselych@seznam.cz>
2023-08-07 16:00:23 +0200

    style(tests/docker_compose): more descriptive variable names

--- .dagger-ci/daggerci/tests/test_docker_compose.py
+++ .dagger-ci/daggerci/tests/test_docker_compose.py
@@ -53,15 +53,15 @@ def test__docker_compose(tmpdir, create_file, docker_compose_file_complex):
         "coreboot_4.19",
         "coreboot_4.20",
         "edk2",
-        "meh",
-        "meh2",
+        "dummy",
+        "dummy2",
     ]
     assert my_docker_compose.get_dockerfiles(top_element="services") == [
         "coreboot_4.19",
         "coreboot_4.20",
         "edk2",
-        "meh",
-        "meh2",
+        "dummy",
+        "dummy2",
     ]
 
     assert my_docker_compose.__select_dockerfile__() == "coreboot_4.19"
@@ -82,8 +82,8 @@ def test__docker_compose(tmpdir, create_file, docker_compose_file_complex):
         == "coreboot"
     )
     assert my_docker_compose.get_dockerfile_context("coreboot_4.20") is None
-    assert my_docker_compose.get_dockerfile_context("meh") is None
-    assert my_docker_compose.get_dockerfile_context("meh2") is None
+    assert my_docker_compose.get_dockerfile_context("dummy") is None
+    assert my_docker_compose.get_dockerfile_context("dummy2") is None
 
     assert my_docker_compose.get_dockerfile_args() == [
         dagger.api.gen.BuildArg("COREBOOT_VERSION", "4.19")
