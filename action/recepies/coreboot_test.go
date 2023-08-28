3404a3bf1387ba20527f8121b1360682083108f4
AtomicFS <vojtaveselych@seznam.cz>
2023-08-28 19:20:05 +0200

    refactor(action): complete refactor of coreboot and recepies (WIP)
    - recepies: contains data-structures and shared functions
    - coreboot: greatly reduced in size, heavily uses action/container and action/recepies
    - add tests to coreboot and recepies
    - might need more work since linux kernel and coreboot can share a lot
      of code

---
+++ action/recepies/coreboot_test.go
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: MIT
+
+// Package recepies / coreboot
+package recepies
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+
+	"dagger.io/dagger"
+	"github.com/9elements/firmware-action/action/container"
+	"github.com/9elements/firmware-action/action/filesystem"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCoreboot(t *testing.T) {
+	// This test is really slow (like 100 seconds)
+	if testing.Short() {
+		t.Skip("skipping test in short mode")
+	}
+
+	const corebootVersion = "4.19"
+	ctx := context.Background()
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
+	assert.NoError(t, err)
+	defer client.Close()
+
+	// Prepare options
+	tmpDir := t.TempDir()
+	opts := map[string]string{
+		"target":           "coreboot",
+		"sdk_version":      fmt.Sprintf("coreboot_%s:main", corebootVersion),
+		"repo_path":        filepath.Join(tmpDir, "coreboot"),
+		"defconfig_path":   "defconfig",
+		"containerWorkDir": "/coreboot",
+		"GITHUB_WORKSPACE": "/coreboot",
+		"output":           "output",
+	}
+	getFunc := func(key string) string {
+		return opts[key]
+	}
+	common, err := commonGetOpts(getFunc)
+	assert.NoError(t, err)
+	corebootOpts := corebootOpts{}
+
+	// Change current working directory
+	pwd, err := os.Getwd()
+	assert.NoError(t, err)
+	err = os.Chdir(tmpDir)
+	assert.NoError(t, err)
+
+	// Clone coreboot repo
+	cmd := exec.Command("git", "clone", "--branch", corebootVersion, "--depth", "1", "https://review.coreboot.org/coreboot")
+	err = cmd.Run()
+	assert.NoError(t, err)
+	err = os.Chdir(common.repoPath)
+	assert.NoError(t, err)
+
+	// Copy over defconfig file into tmpDir
+	defconfigPath := filepath.Join(common.repoPath, "defconfig")
+	err = filesystem.CopyFile(
+		filepath.Join(pwd, fmt.Sprintf("../../tests/coreboot_%s/seabios.defconfig", corebootVersion)),
+		defconfigPath,
+	)
+	// ^^^ this relative path might be funky
+	assert.NoError(t, err)
+
+	// Artifacts
+	artifacts := []container.Artifacts{
+		{
+			ContainerPath: filepath.Join(common.containerWorkDir, "build", "coreboot.rom"),
+			ContainerDir:  false,
+			HostPath:      tmpDir,
+		},
+		{
+			ContainerPath: filepath.Join(common.containerWorkDir, "defconfig"),
+			ContainerDir:  false,
+			HostPath:      tmpDir,
+		},
+	}
+
+	// Try to build coreboot
+	err = coreboot(ctx, client, &common, &corebootOpts, &artifacts)
+	assert.NoError(t, err)
+
+	// Check artifects
+	assert.ErrorIs(t, filesystem.CheckFileExists(filepath.Join(tmpDir, "coreboot.rom")), os.ErrExist)
+	assert.ErrorIs(t, filesystem.CheckFileExists(filepath.Join(tmpDir, "defconfig")), os.ErrExist)
+}
--- action/recepies/recepies.go
+++ action/recepies/recepies.go
@@ -1,94 +1,159 @@
 // SPDX-License-Identifier: MIT
 
-// Package main implements the core logic of running composable Dagger pipelines
-// via GitHub Actions. Currently supported are coreboot and Linux pipelines.
+// Package recepies yay!
 package recepies
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"net/url"
+	"path"
+	"path/filepath"
+	"strings"
 
 	"dagger.io/dagger"
-	"github.com/9elements/firmware-action/action/kconfig"
+	"github.com/9elements/firmware-action/action/container"
 	"github.com/sethvargo/go-githubactions"
 )
 
+//===================
+// Universal options
+//===================
+
+var errRequiredOptionUndefined = errors.New("required option is undefined")
+
+type (
+	getValFunc func(string) string
+)
+
+// commontOpts is common to all targets
+// Used to store data from githubaction.Action
+// For details see action.yml
+type commonOpts struct {
+	target           string
+	sdkVersion       string
+	repoPath         string
+	defconfigPath    string
+	containerWorkDir string
+	outputDir        string
+}
+
+// commonGetOpts is used to fill commontOpts with data from githubaction.Action
+func commonGetOpts(get getValFunc) (commonOpts, error) {
+	opts := commonOpts{
+		target:           get("target"),
+		sdkVersion:       get("sdk_version"),
+		repoPath:         get("repo_path"),
+		defconfigPath:    get("defconfig_path"),
+		containerWorkDir: get("GITHUB_WORKSPACE"),
+		outputDir:        get("output"),
+	}
+
+	// Check if required options are not empty
+	missing := []string{}
+	requiredOptions := map[string]string{
+		"target":           opts.target,
+		"sdk_version":      opts.sdkVersion,
+		"repo_path":        opts.repoPath,
+		"defconfig_path":   opts.defconfigPath,
+		"containerWorkDir": opts.containerWorkDir,
+		"output":           opts.outputDir,
+	}
+	for key, val := range requiredOptions {
+		if val == "" {
+			missing = append(missing, key)
+		}
+	}
+	if len(missing) > 0 {
+		return opts, fmt.Errorf("%w: %s", errRequiredOptionUndefined, strings.Join(missing, ", "))
+	}
+
+	// Check if sdk_version is URL, if not make it into URL defaulting to our containers
+	_, err := url.ParseRequestURI(opts.sdkVersion)
+	if err != nil {
+		opts.sdkVersion = path.Join("ghcr.io/9elements/firmware-action", opts.sdkVersion)
+	}
+	return opts, nil
+}
+
+//==========
+// COREBOOT
+//==========
+
+// Used to store data from githubaction.Action
+// For details see action.yml
+type corebootOpts struct {
+	payloadFilePath  string
+	blobIntelIfdPath string
+	blobIntelMePath  string
+	blobIntelGbePath string
+	fspBinaryPath    string
+	fspHeaderPath    string
+}
+
+// commonGetOpts is used to fill corebootOpts with data from githubaction.Action
+func corebootGetOpts(get getValFunc) (corebootOpts, error) {
+	opts := corebootOpts{
+		payloadFilePath:  get("coreboot__payload_file_path"),
+		blobIntelIfdPath: get("coreboot__blob_intel_ifd_path"),
+		blobIntelMePath:  get("coreboot__blob_intel_me_path"),
+		blobIntelGbePath: get("coreboot__blob_intel_gbe_path"),
+		fspBinaryPath:    get("coreboot__fsp_binary_path"),
+		fspHeaderPath:    get("coreboot__fsp_header_path"),
+	}
+
+	// Check if required options are not empty
+	// ... I don't think any of these are always required, might depend on provided defconfig
+	return opts, nil
+}
+
+//=======
+// LINUX
+//=======
+
+//======
+// EDK2
+//======
+
+//=====================
+// Universal Functions
+//=====================
+
+// Execute recepie
 func Execute(ctx context.Context, client *dagger.Client, action *githubactions.Action) error {
 	switch action.GetInput("target") {
 	case "coreboot":
-		return coreboot(ctx, action, client)
-	case "linux":
-		return linux(ctx, action, client)
+		common, err := commonGetOpts(action.GetInput)
+		if err != nil {
+			return err
+		}
+		opts, err := corebootGetOpts(action.GetInput)
+		if err != nil {
+			return err
+		}
+		artifacts := []container.Artifacts{
+			{
+				ContainerPath: filepath.Join(common.containerWorkDir, "build", "coreboot.rom"),
+				ContainerDir:  false,
+				HostPath:      common.outputDir,
+			},
+			{
+				ContainerPath: filepath.Join(common.containerWorkDir, "defconfig"),
+				ContainerDir:  false,
+				HostPath:      common.outputDir,
+			},
+		}
+		return coreboot(ctx, client, &common, &opts, &artifacts)
+	/*
+		case "linux":
+			return linux(ctx, action, client)
+		case "edk2":
+			return edk2(ctx, action, client)
+	*/
 	case "":
 		return fmt.Errorf("no target specified")
 	default:
 		return fmt.Errorf("unsupported target: %s", action.GetInput("target"))
 	}
-}
-
-func generateDotConfigFromDefconfig(ctx context.Context, action *githubactions.Action, client *dagger.Client, defconfig string) (*kconfig.Kconfig, error) {
-	corebootContainer, err := setupCorebootContainer(ctx, action, client, defconfig)
-	if err != nil {
-		return nil, fmt.Errorf("Failed to read .config: %v", err)
-	}
-
-	// generate .config
-	corebootContainer = corebootContainer.WithExec([]string{"rm", ".config"})
-	corebootContainer, err = corebootContainer.Sync(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("Error during execution: %v", err)
-	}
-
-	o, err := corebootContainer.Stdout(ctx)
-	if o != "" {
-		fmt.Println(o)
-	}
-	if err != nil {
-		return nil, err
-	}
-
-	o, err = corebootContainer.Stderr(ctx)
-	if o != "" {
-		fmt.Println(o)
-	}
-	if err != nil {
-		return nil, err
-	}
-
-	corebootContainer = corebootContainer.WithExec([]string{"make", "defconfig", "KBUILD_DEFCONFIG=defconfig"})
-	corebootContainer, err = corebootContainer.Sync(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("Error during execution: %v", err)
-	}
-
-	o, err = corebootContainer.Stdout(ctx)
-	if o != "" {
-		fmt.Println(o)
-	}
-	if err != nil {
-		return nil, err
-	}
-
-	o, err = corebootContainer.Stderr(ctx)
-	if o != "" {
-		fmt.Println(o)
-	}
-	if err != nil {
-		return nil, err
-	}
-
-	// Extract .config
-	rom := corebootContainer.File(".config")
-	dotconfigRaw, err := rom.Contents(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("Failed to read .config: %v", err)
-	}
-
-	var dotConfig *kconfig.Kconfig
-	dotConfig, err = kconfig.NewKconfig(dotconfigRaw)
-	if err != nil {
-		return nil, fmt.Errorf("Failed to convert .config: %v", err)
-	}
-
-	return dotConfig, nil
 }
