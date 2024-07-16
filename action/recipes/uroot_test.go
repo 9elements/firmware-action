ed3f45cf288a70041161420a46370569d3e6bfac
AtomicFS <vojtech.vesely@9elements.com>
2024-07-16 16:46:02 +0200

    fix(action): simplify u-root test
    Signed-off-by: AtomicFS <vojtech.vesely@9elements.com>

--- action/recipes/uroot_test.go
+++ action/recipes/uroot_test.go
@@ -76,7 +76,7 @@ func TestURoot(t *testing.T) {
 
 			myURootOpts := URootOpts
 			myURootOpts.SdkURL = fmt.Sprintf("golang:%s", tc.golangVersion)
-			myURootOpts.BuildCommand = fmt.Sprintf("go build; GOARCH=%s ./u-root -defaultsh gosh -o initramfs.cpio boot coreboot-app ./cmds/core/* ./cmds/boot/*", tc.arch)
+			myURootOpts.BuildCommand = fmt.Sprintf("go build; GOARCH=%s ./u-root -o initramfs.cpio core boot", tc.arch)
 			myURootOpts.RepoPath = filepath.Join(tmpDir, "u-root")
 
 			// Change current working directory
