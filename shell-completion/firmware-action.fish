a957353e182a154d0955ac876b778bcad135c90a
AtomicFS <vojtech.vesely@9elements.com>
2024-11-21 16:19:57 +0100

    feat(action): add shell completion
    Signed-off-by: AtomicFS <vojtech.vesely@9elements.com>

---
+++ shell-completion/firmware-action.fish
@@ -0,0 +1,97 @@
+function _firmware-action_spec_2
+set 1 $argv[1]
+__fish_complete_path "$1"
+end
+
+function _firmware-action
+    set COMP_LINE (commandline --cut-at-cursor)
+
+    set COMP_WORDS
+    echo $COMP_LINE | read --tokenize --array COMP_WORDS
+    if string match --quiet --regex '.*\s$' $COMP_LINE
+        set COMP_CWORD (math (count $COMP_WORDS) + 1)
+    else
+        set COMP_CWORD (count $COMP_WORDS)
+    end
+
+    set --local literals "generate-config" "--config" "--json" "build" "version" "--indent" "--debug" "--target" "--recursive" "--interactive" "--help"
+
+    set --local descriptions
+
+    set --local literal_transitions
+    set literal_transitions[1] "set inputs 1 4 5 11; set tos 2 3 4 4"
+    set literal_transitions[2] "set inputs 2 11; set tos 5 4"
+    set literal_transitions[3] "set inputs 6 2 3 7 8 9 10 11; set tos 7 8 7 7 6 7 7 4"
+    set literal_transitions[7] "set inputs 6 2 3 7 8 9 10; set tos 7 8 7 7 6 7 7"
+
+    set --local match_anything_transitions_from 6 5 8
+    set --local match_anything_transitions_to 7 4 7
+
+    set --local state 1
+    set --local word_index 2
+    while test $word_index -lt $COMP_CWORD
+        set --local -- word $COMP_WORDS[$word_index]
+
+        if set --query literal_transitions[$state] && test -n $literal_transitions[$state]
+            set --local --erase inputs
+            set --local --erase tos
+            eval $literal_transitions[$state]
+
+            if contains -- $word $literals
+                set --local literal_matched 0
+                for literal_id in (seq 1 (count $literals))
+                    if test $literals[$literal_id] = $word
+                        set --local index (contains --index -- $literal_id $inputs)
+                        set state $tos[$index]
+                        set word_index (math $word_index + 1)
+                        set literal_matched 1
+                        break
+                    end
+                end
+                if test $literal_matched -ne 0
+                    continue
+                end
+            end
+        end
+
+        if set --query match_anything_transitions_from[$state] && test -n $match_anything_transitions_from[$state]
+            set --local index (contains --index -- $state $match_anything_transitions_from)
+            set state $match_anything_transitions_to[$index]
+            set word_index (math $word_index + 1)
+            continue
+        end
+
+        return 1
+    end
+
+    if set --query literal_transitions[$state] && test -n $literal_transitions[$state]
+        set --local --erase inputs
+        set --local --erase tos
+        eval $literal_transitions[$state]
+        for literal_id in $inputs
+            if test -n $descriptions[$literal_id]
+                printf '%s\t%s\n' $literals[$literal_id] $descriptions[$literal_id]
+            else
+                printf '%s\n' $literals[$literal_id]
+            end
+        end
+    end
+
+    set specialized_command_states 5 8
+    set specialized_command_ids 2 2
+    if contains $state $specialized_command_states
+        set --local index (contains --index $state $specialized_command_states)
+        set --local function_id $specialized_command_ids[$index]
+        set --local function_name _firmware-action_spec_$function_id
+        set --local --erase inputs
+        set --local --erase tos
+        set --local lines (eval $function_name $COMP_WORDS[$COMP_CWORD])
+        for line in $lines
+            printf '%s\n' $line
+        end
+    end
+
+    return 0
+end
+
+complete --command firmware-action --no-files --arguments "(_firmware-action)"
