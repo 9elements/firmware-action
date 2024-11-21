8fe18fe0969aae84cd0340b95c84c7f3534e80fd
AtomicFS <vojtech.vesely@9elements.com>
2024-11-21 16:19:57 +0100

    feat(action): add shell completion
    Signed-off-by: AtomicFS <vojtech.vesely@9elements.com>

---
+++ shell-completion/firmware-action.bash
@@ -0,0 +1,107 @@
+_firmware-action_spec_1 () {
+    compgen -A file "$1"
+}
+
+_firmware-action () {
+    if [[ $(type -t _get_comp_words_by_ref) != function ]]; then
+        echo _get_comp_words_by_ref: function not defined.  Make sure the bash-completions system package is installed
+        return 1
+    fi
+
+    local words cword
+    _get_comp_words_by_ref -n "$COMP_WORDBREAKS" words cword
+
+    local -a literals=("generate-config" "--config" "--json" "build" "version" "--indent" "--debug" "--target" "--recursive" "--interactive" "--help")
+
+    declare -A literal_transitions
+    literal_transitions[0]="([0]=1 [3]=2 [4]=3 [10]=3)"
+    literal_transitions[1]="([1]=4 [10]=3)"
+    literal_transitions[2]="([5]=6 [1]=7 [2]=6 [6]=6 [7]=5 [8]=6 [9]=6 [10]=3)"
+    literal_transitions[6]="([5]=6 [1]=7 [2]=6 [6]=6 [7]=5 [8]=6 [9]=6)"
+
+    declare -A match_anything_transitions
+    match_anything_transitions=([5]=6 [4]=3 [7]=6)
+    declare -A subword_transitions
+
+    local state=0
+    local word_index=1
+    while [[ $word_index -lt $cword ]]; do
+        local word=${words[$word_index]}
+
+        if [[ -v "literal_transitions[$state]" ]]; then
+            declare -A state_transitions
+            eval "state_transitions=${literal_transitions[$state]}"
+
+            local word_matched=0
+            for literal_id in $(seq 0 $((${#literals[@]} - 1))); do
+                if [[ ${literals[$literal_id]} = "$word" ]]; then
+                    if [[ -v "state_transitions[$literal_id]" ]]; then
+                        state=${state_transitions[$literal_id]}
+                        word_index=$((word_index + 1))
+                        word_matched=1
+                        break
+                    fi
+                fi
+            done
+            if [[ $word_matched -ne 0 ]]; then
+                continue
+            fi
+        fi
+
+        if [[ -v "match_anything_transitions[$state]" ]]; then
+            state=${match_anything_transitions[$state]}
+            word_index=$((word_index + 1))
+            continue
+        fi
+
+        return 1
+    done
+
+
+    local prefix="${words[$cword]}"
+
+    local shortest_suffix="$word"
+    for ((i=0; i < ${#COMP_WORDBREAKS}; i++)); do
+        local char="${COMP_WORDBREAKS:$i:1}"
+        local candidate="${word##*$char}"
+        if [[ ${#candidate} -lt ${#shortest_suffix} ]]; then
+            shortest_suffix=$candidate
+        fi
+    done
+    local superfluous_prefix=""
+    if [[ "$shortest_suffix" != "$word" ]]; then
+        local superfluous_prefix=${word%$shortest_suffix}
+    fi
+
+    if [[ -v "literal_transitions[$state]" ]]; then
+        local state_transitions_initializer=${literal_transitions[$state]}
+        declare -A state_transitions
+        eval "state_transitions=$state_transitions_initializer"
+
+        for literal_id in "${!state_transitions[@]}"; do
+            local literal="${literals[$literal_id]}"
+            if [[ $literal = "${prefix}"* ]]; then
+                local completion=${literal#"$superfluous_prefix"}
+                COMPREPLY+=("$completion ")
+            fi
+        done
+    fi
+
+    declare -A specialized_commands
+    specialized_commands=([4]=1 [7]=1)
+    if [[ -v "specialized_commands[$state]" ]]; then
+        local command_id=${specialized_commands[$state]}
+        local completions=()
+        mapfile -t completions < <(_firmware-action_spec_"${command_id}" "$prefix" | cut -f1)
+        for item in "${completions[@]}"; do
+            if [[ $item = "${prefix}"* ]]; then
+                COMPREPLY+=("$item")
+            fi
+        done
+    fi
+
+
+    return 0
+}
+
+complete -o nospace -F _firmware-action firmware-action
---
+++ shell-completion/firmware-action.zsh
@@ -0,0 +1,119 @@
+_firmware-action_spec_1 () {
+    _path_files
+}
+
+_firmware-action () {
+    local -a literals=("generate-config" "--config" "--json" "build" "version" "--indent" "--debug" "--target" "--recursive" "--interactive" "--help")
+
+    local -A descriptions
+
+    local -A literal_transitions
+    literal_transitions[1]="([1]=2 [4]=3 [5]=4 [11]=4)"
+    literal_transitions[2]="([2]=5 [11]=4)"
+    literal_transitions[3]="([6]=7 [2]=8 [3]=7 [7]=7 [8]=6 [9]=7 [10]=7 [11]=4)"
+    literal_transitions[7]="([6]=7 [2]=8 [3]=7 [7]=7 [8]=6 [9]=7 [10]=7)"
+
+    local -A match_anything_transitions
+    match_anything_transitions=([6]=7 [5]=4 [8]=7)
+
+    declare -A subword_transitions
+
+    local state=1
+    local word_index=2
+    while [[ $word_index -lt $CURRENT ]]; do
+        if [[ -v "literal_transitions[$state]" ]]; then
+            local -A state_transitions
+            eval "state_transitions=${literal_transitions[$state]}"
+
+            local word=${words[$word_index]}
+            local word_matched=0
+            for ((literal_id = 1; literal_id <= $#literals; literal_id++)); do
+                if [[ ${literals[$literal_id]} = "$word" ]]; then
+                    if [[ -v "state_transitions[$literal_id]" ]]; then
+                        state=${state_transitions[$literal_id]}
+                        word_index=$((word_index + 1))
+                        word_matched=1
+                        break
+                    fi
+                fi
+            done
+            if [[ $word_matched -ne 0 ]]; then
+                continue
+            fi
+        fi
+
+        if [[ -v "match_anything_transitions[$state]" ]]; then
+            state=${match_anything_transitions[$state]}
+            word_index=$((word_index + 1))
+            continue
+        fi
+
+        return 1
+    done
+
+    completions_no_description_trailing_space=()
+    completions_no_description_no_trailing_space=()
+    completions_trailing_space=()
+    suffixes_trailing_space=()
+    descriptions_trailing_space=()
+    completions_no_trailing_space=()
+    suffixes_no_trailing_space=()
+    descriptions_no_trailing_space=()
+
+    if [[ -v "literal_transitions[$state]" ]]; then
+        local -A state_transitions
+        eval "state_transitions=${literal_transitions[$state]}"
+
+        for literal_id in ${(k)state_transitions}; do
+            if [[ -v "descriptions[$literal_id]" ]]; then
+                completions_trailing_space+=("${literals[$literal_id]}")
+                suffixes_trailing_space+=("${literals[$literal_id]}")
+                descriptions_trailing_space+=("${descriptions[$literal_id]}")
+            else
+                completions_no_description_trailing_space+=("${literals[$literal_id]}")
+            fi
+        done
+    fi
+
+    local -A specialized_commands=([5]=1 [8]=1)
+    if [[ -v "specialized_commands[$state]" ]]; then
+        local command_id=${specialized_commands[$state]}
+        _firmware-action_spec_${command_id} ${words[$CURRENT]}
+    fi
+
+    local maxlen=0
+    for suffix in ${suffixes_trailing_space[@]}; do
+        if [[ ${#suffix} -gt $maxlen ]]; then
+            maxlen=${#suffix}
+        fi
+    done
+    for suffix in ${suffixes_no_trailing_space[@]}; do
+        if [[ ${#suffix} -gt $maxlen ]]; then
+            maxlen=${#suffix}
+        fi
+    done
+
+    for ((i = 1; i <= $#suffixes_trailing_space; i++)); do
+        if [[ -z ${descriptions_trailing_space[$i]} ]]; then
+            descriptions_trailing_space[$i]="${(r($maxlen)( ))${suffixes_trailing_space[$i]}}"
+        else
+            descriptions_trailing_space[$i]="${(r($maxlen)( ))${suffixes_trailing_space[$i]}} -- ${descriptions_trailing_space[$i]}"
+        fi
+    done
+
+    for ((i = 1; i <= $#suffixes_no_trailing_space; i++)); do
+        if [[ -z ${descriptions_no_trailing_space[$i]} ]]; then
+            descriptions_no_trailing_space[$i]="${(r($maxlen)( ))${suffixes_no_trailing_space[$i]}}"
+        else
+            descriptions_no_trailing_space[$i]="${(r($maxlen)( ))${suffixes_no_trailing_space[$i]}} -- ${descriptions_no_trailing_space[$i]}"
+        fi
+    done
+
+    compadd -Q -a completions_no_description_trailing_space
+    compadd -Q -S ' ' -a completions_no_description_no_trailing_space
+    compadd -l -Q -a -d descriptions_trailing_space completions_trailing_space
+    compadd -l -Q -S '' -a -d descriptions_no_trailing_space completions_no_trailing_space
+    return 0
+}
+
+compdef _firmware-action firmware-action
