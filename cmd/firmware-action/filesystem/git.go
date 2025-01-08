2aebc88d9c851dcb401a948affbd928a2baad6f1
AtomicFS <vojtech.vesely@9elements.com>
2025-01-08 21:26:23 +0100

    feat(cmd): add functions for handling git describe
    - the motivations behind this:
    - coreboot likes to embed its source version into the compiled binary
    - coreboot does that by calling 'git describe ...'
    - however with firmware-action we can assume people will include
      coreboot as git submodule, and thus when compiling coreboot in
      container the git commands will fail because of missing .git directory
    - to fix this we will run manually the 'git describe' command and then
      pass it in form of env variable into the container
    
    Signed-off-by: AtomicFS <vojtech.vesely@9elements.com>

---
+++ cmd/firmware-action/filesystem/git.go
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+
+// Package filesystem / git implements git-related commands
+package filesystem
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+// gitRun is generic function to execute any git command in sub-directory
+func gitRun(subdir string, command []string) (string, error) {
+	// subdir is relative to current working directory
+
+	// Get current working directory
+	pwd, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+
+	// Change current working directory into the repository / submodule
+	subdirPath := filepath.Join(pwd, subdir)
+	defer os.Chdir(pwd) // nolint:errcheck
+	err = os.Chdir(subdirPath)
+	if err != nil {
+		slog.Error(
+			fmt.Sprintf("Failed to change current working directory to '%s'", subdirPath),
+			slog.Any("error", err),
+		)
+		return "", err
+	}
+
+	// Run git describe
+	cmd := exec.Command(command[0], command[1:]...)
+	describe, err := cmd.CombinedOutput()
+	if err != nil {
+		slog.Error(
+			fmt.Sprintf("Failed to run git command in '%s'", subdirPath),
+			slog.Any("error", err),
+		)
+		return "", err
+	}
+
+	return string(describe), nil
+}
+
+// GitDescribeCoreboot is coreboot-specific git describe command
+func GitDescribeCoreboot(repoPath string) (string, error) {
+	describe, err := gitRun(repoPath, []string{"git", "describe", "--abbrev=12", "--dirty", "--always"})
+	if err != nil {
+		return "", err
+	}
+
+	// Remove trailing newline
+	result := strings.TrimSpace(describe)
+
+	// Check validity of the returned string
+	pattern := regexp.MustCompile(`[\d\w]{13}(\-dirty)?`)
+	valid := pattern.MatchString(result)
+	if !valid {
+		slog.Warn(
+			fmt.Sprintf("Output of 'git describe' for '%s' seems to be invalid, output is: '%s'", repoPath, result),
+		)
+	}
+
+	return result, err
+}
