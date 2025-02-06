cd3ced950dbb1be4765fb4b5c323460a85ae2c5e
AtomicFS <vojtech.vesely@9elements.com>
2025-02-06 14:54:05 +0100

    chore(docker): update get_env_var_value
    Signed-off-by: AtomicFS <vojtech.vesely@9elements.com>

--- .dagger-ci/daggerci/lib/env.py
+++ .dagger-ci/daggerci/lib/env.py
@@ -5,10 +5,13 @@ Functions to deal with environment variables
 import os
 
 
-def get_env_var_value(list_of_maybe_existing_env_vars: list[str], fallback: str) -> str:
+def get_env_var_value(
+    list_of_maybe_existing_env_vars: list[str], fallback: str | None
+) -> str | None:
     """
     Get a value of first defined environment variable found in "list_of_maybe_existing_env_vars",
     or get "fallback".
+    I often use "fallback" as 'None'
     """
     for i in list_of_maybe_existing_env_vars:
         if os.environ.get(i):
