59abdd03224005ff5a077f00b19f3467ba8a72f6
AtomicFS <vojtech.vesely@9elements.com>
2026-08-04 16:21:55 +0200

    chore(uboot): switch to GitHub mirror
    - the source.denx.de server seems to be rather unstable and slow
    - let's switch to GitHub mirror instead
    
    Signed-off-by: AtomicFS <vojtech.vesely@9elements.com>

--- tests/test_uboot.sh
+++ tests/test_uboot.sh
@@ -3,7 +3,7 @@
 set -Eeuo pipefail
 
 # Clone repo
-git clone https://source.denx.de/u-boot/u-boot.git
+git clone https://github.com/u-boot/u-boot.git
 cd u-boot
 git fetch -a
 git checkout "${VERIFICATION_TEST_UBOOT_VERSION}"
